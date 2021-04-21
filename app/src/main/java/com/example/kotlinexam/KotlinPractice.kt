45045ff8456d69cb37ed45831d0b4ff9c8bebfdf
LucyMoon <rjf5907@daum.net>
2021-04-21 14:59:37 +0900

    Test Commit

--- app/src/main/java/com/example/kotlinexam/KotlinPractice.kt
+++ app/src/main/java/com/example/kotlinexam/KotlinPractice.kt
@@ -2,4 +2,18 @@ package com.example.kotlinexam
 
 
 // 1.Lamda
-//람다식은 우리가 마치 value 처럼 다룰 수 있는 익명함수이다.
+// 람다식은 우리가 마치 value 처럼 다룰 수 있는 익명함수이다.
+// 1) 메소드의 파라미터로 넘겨줄 수가 있다.  fun maxBy(a : Int)
+// 2) return 값으로 사용할 수가 있다.
+
+// 람다의 기본 정의
+// val lamdaName : Type = {argumentList -> codeBody}
+
+
+val square :  (Int) -> (Int) ={number -> number*number}
+
+
+
+fun main(){
+    println(square(12))
+}
